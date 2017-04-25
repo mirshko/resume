304d80e6b086a7684d792dd1ff7c845856227b4c
Jeff Reiner <theundeadghost@gmail.com>
2017-04-25 09:56:56 -0400

    :tada: Rebuilding resume.reiner.io in Spike 1.0
    Signed-off-by: Jeff Reiner <theundeadghost@gmail.com>

---
+++ locals/resume.js
@@ -0,0 +1,92 @@
+module.exports = {
+  name: 'Jeff Reiner',
+  title: 'Marketer / Designer / Front-end Developer',
+  about: 'Hello, I\'m a web developer & designer based in Boston. I work at Onshape, building and designing the marketing website.',
+  work: [
+    {
+      company: 'Onshape',
+      website: 'http://onshape.com',
+      title: 'Web Developer',
+      timeframe: 'Jan, 2017 – Current',
+      summary: 'Primary development & upkeep of marketing websites; both English and international'
+    },
+    {
+      company: 'Onshape',
+      website: 'http://onshape.com',
+      title: 'Marketing Specialist',
+      timeframe: 'Jan, 2016 – Jan, 2017'
+    },
+    {
+      company: 'Onshape',
+      website: 'http://onshape.com',
+      title: 'Marketing Intern',
+      timeframe: 'Jun, 2014 – Dec, 2015'
+    },
+    {
+      company: 'The MGH/HST Martinos Center for Biomedical Imaging',
+      website: 'https://www.nmr.mgh.harvard.edu/',
+      title: 'Linux Systems Administrator',
+      timeframe: 'Jun, 2013 – August, 2013'
+    }
+  ],
+  education: [
+    {
+      college: 'Becker College',
+      degree: 'Bachelor\'s',
+      major: 'Game and Interactive Media Design',
+      timeframe: '2013 - 2015',
+      accomplishments: [
+        'Graduated Summa Cum Laude',
+        'Alpha Chi Honor Society',
+        '3.96 GPA'
+      ]
+    }
+  ],
+  projects: [
+    {
+      title: 'Wistia Thumbnail Generator',
+      role: 'Creator',
+      website: 'http://thumbnailer.reiner.space',
+      summary: 'Easy to use tool to get thumbnails (and their URLs) directly from Wistia videos easily in any size.'
+    },
+    {
+      title: 'Onshape.com',
+      role: 'Lead Developer / Designer',
+      website: 'http://onshape.com/',
+      summary: 'I took over ownership of the website and rebuilt it from scratch with Materialize CSS as a framework. In the process I taught myself JavaScript, SASS, Gulp, and jQuery, along with engineering a GitHub to HubSpot build platform that gave us version control and other benefits. The Onshape website is not only cleaner in terms of code, it is also faster, 100% more responsive, and has a better UX than our previous website.'
+    },
+    {
+      title: 'Noti',
+      role: 'Designer',
+      website: 'https://noti.center/',
+      summary: 'I took on the initiative to redesign the Noti preferences panel as well as the primary app icon and brand mascot. I also designed the upcoming SMS view which is currently under development.'
+    }
+  ],
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Sketch App',
+    'Adobe Suite',
+    'Gulp',
+    'HubSpot',
+    'Marketing',
+    'Design',
+    'Wistia',
+    'Google Analytics',
+    'Jira',
+    'Git',
+    'SEO',
+    'Transifex (i18n)',
+    'Google Webmaster Tools',
+    'HubL (HubSpot Templating)',
+    'Nunjucks'
+  ],
+  contact: {
+    email: 'jeff@reiner.io',
+    telephone: '9783195997',
+    website: 'http://reiner.io',
+    city: 'Boston',
+    state: 'MA'
+  }
+}
